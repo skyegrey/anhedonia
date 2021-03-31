44a3a8f658c0efe307d61f89a38c24546659a814
skyegrey <dev.skyegrey@gmail.com>
2021-03-31 17:27:10 -0500

    Added purchasing power & scoring skeleton

--- nodes/root_node.py
+++ nodes/root_node.py
@@ -7,6 +7,7 @@ class RootNode(FunctionNode):
 
     def __init__(self, child_nodes):
         super().__init__(child_nodes)
+        self.purchasing_power = 1000
     
     def get_decision(self):
         return self.calculate()
